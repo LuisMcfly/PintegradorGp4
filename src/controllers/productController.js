a362bb1afca0b9222f4511f6047f77757fec7aa3
JMWalkman <josemanuel.tmn@gmail.com>
2022-09-22 18:14:08 -0500

    Modularización de Head, Header, y Footer

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,7 @@
 const productController = {
-    productDetail: (req, res) => res.render('products/productDetail')
+    productDetail: (req, res) => res.render('products/productDetail'),
+    productRegister: (req, res) => res.render('products/productRegister'),
+    productEdit: (req, res) => res.render('products/productEdit'),
 }
 
 module.exports = productController;
--- src/routes/product.js
+++ src/routes/product.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/productDetail', productController.productDetail);
+router.get('/productRegister', productController.productRegister);
+router.get('/productEdit', productController.productEdit);
 
 module.exports = router;
