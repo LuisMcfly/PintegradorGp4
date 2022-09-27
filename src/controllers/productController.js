9de37aaad88e208eb84365599482a8e3ac4cd503
JMWalkman <josemanuel.tmn@gmail.com>
2022-09-27 18:31:48 -0500

    productEdit y productRegister de Jorge

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,7 @@ const productController = {
     productDetail: (req, res) => res.render('products/productDetail'),
     productRegister: (req, res) => res.render('products/productRegister'),
     productEdit: (req, res) => res.render('products/productEdit'),
+    productRegisterConclude: (req, res) => res.render('products/productRegisterConclude'),
 }
 
 module.exports = productController;
