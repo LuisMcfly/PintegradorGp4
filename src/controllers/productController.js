4406b22f40652477e3021c0d8ea8951fa81a7a1f
hernando-code <hernaranjo@gmail.com>
2022-10-04 19:10:32 -0500

    Formularios de registro

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,32 @@
+let fs = require('fs')
+const { uuid } = require('uuidv4')
+const fabricantes = [{id: 1, name:'Lenovo'}, {id:2, name:'Asus'}, {id:3, name:'Dell'}, {id:4,name:'Logitec'}, {id:5,name:'RedDragon'}]
 const productController = {
     productDetail: (req, res) => res.render('products/productDetail'),
-    productRegister: (req, res) => res.render('products/productRegister'),
+    productRegister: (req, res) => res.render('products/productRegister', {fabricantes:fabricantes}),
+    create: (req, res) => {
+        let image 
+        if(req.files[0]!=undefined){
+            image=req.files[0].filename
+        }else{
+            image = ''
+        }
+
+        let product = 
+            {id: uuid(),...req.body, image},
+         products = '',
+         productsFile = fs.readFileSync('products.json', {encoding: 'utf-8'})//trae lo que hay en en archivo, se parsea para poder usarlo en js
+        if(productsFile == ''){
+             products = []
+        }else{
+             products = JSON.parse(productsFile)
+        }
+        products.push(product)
+        
+        let productsJSON = JSON.stringify(products, null)
+        fs.writeFileSync('products.json', productsJSON)
+        res.redirect('productRegisterConclude')
+    },
     productEdit: (req, res) => res.render('products/productEdit'),
     productRegisterConclude: (req, res) => res.render('products/productRegisterConclude'),
 }
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 const productController = require('../controllers/productController');
+/* */
+const multer = require('multer'),
+    storage =  multer.diskStorage({
+        destination: (req, file, cb)=>{
+            cb(null, path.join('public','img','uploads'))
+        },
+        filename: (req, file, cb)=>{
+            cb(null, file.originalname)
+        }
+    })
+
+
+const upload = multer({storage:storage})
+
+router.get('/productRegister', productController.productRegister)//renderiza la vista del form
+router.post('/productRegister', upload.any(), productController.create)//guarda los datos enviados en el form
+
+
 
 router.get('/productDetail', productController.productDetail);
-router.get('/productRegister', productController.productRegister);
+
+
+
 router.get('/productEdit', productController.productEdit);
+
 router.get('/productRegisterConclude', productController.productRegisterConclude);
 
 module.exports = router;
