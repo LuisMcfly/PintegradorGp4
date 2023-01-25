153a488ec6d767765da69cd0d69ff4a145a276dc
LuisMcfly <luis.payares.ruiz@gmail.com>
2023-01-24 20:54:21 -0500

    USer Edit full, fix problem product edit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,34 +3,33 @@ const { check, validationResult } = require('express-validator');
 const { Op } = require("sequelize");
 const { Product, Category, Manufacturer, Features } = require('../../models/index');
 const { uploadsPath } = require('../../helpers/filePaths')
-//'products/productShop'
+
 const productShopRender = async (req, res) => {
     const [products] = await Promise.all([Product.findAll({
         where: {
             stock: { [Op.gt]: 0 } // Si no hay stock, no lo muestra en la tienda
         }
     })]);
-    // res.send(products)
 
-    res.render('products/productShop', {products})
+    res.render('products/productShop', { products })
 }
 
-const productDetailRender = async (req, res) => {    
+const productDetailRender = async (req, res) => {
     const { id } = req.params;
 
     // Validacion de que el producto si existe
     const productInfo = await Product.findByPk(id, {
         include: [
-            { model: Manufacturer, as: 'manufacturer'},
-            { model: Category, as: 'category'},
-            { model: Features, as: 'feature'}
+            { model: Manufacturer, as: 'manufacturer' },
+            { model: Category, as: 'category' },
+            { model: Features, as: 'feature' }
         ]
-    }).catch(()=>{
+    }).catch(() => {
         return res.redirect('/');
     });
-    
+
     // return res.send(productInfo)
-    return res.render('products/productDetail', {productInfo, uploadsPath})
+    return res.render('products/productDetail', { productInfo, uploadsPath })
 }
 
 
@@ -44,8 +43,8 @@ const productRegisterRender = async (req, res) => {
     ])
 
     res.render('products/productRegister', {
-        categories, 
-        manufacturers, 
+        categories,
+        manufacturers,
         features,
         errors: [],
         datos: {},
@@ -78,7 +77,7 @@ const productCreate = async (req, res) => {
 
     let resultado = validationResult(req);
 
-    if(!resultado.isEmpty()){
+    if (!resultado.isEmpty()) {
 
         const [categories, manufacturers, features] = await Promise.all([
             Category.findAll(),
@@ -87,7 +86,7 @@ const productCreate = async (req, res) => {
         ])
 
         return res.render('products/productRegister', {
-            categories, 
+            categories,
             manufacturers,
             features,
             errors: resultado.mapped(),
@@ -96,15 +95,15 @@ const productCreate = async (req, res) => {
     };
 
 
-    const { productName, 
-        manufacturer: manufacturer_id, 
-        model, 
-        features: features_id, 
-        category: category_id, 
-        description, 
-        price, 
-        discount, 
-        stock 
+    const { productName,
+        manufacturer: manufacturer_id,
+        model,
+        features: features_id,
+        category: category_id,
+        description,
+        price,
+        discount,
+        stock
     } = req.body
 
     try {
@@ -112,12 +111,12 @@ const productCreate = async (req, res) => {
             name: productName,
             manufacturer_id,
             model,
-            features_id, 
-            category_id, 
-            description, 
-            price, 
-            discount, 
-            stock, 
+            features_id,
+            category_id,
+            description,
+            price,
+            discount,
+            stock,
             images
         })
     } catch (error) {
@@ -130,44 +129,47 @@ const productCreate = async (req, res) => {
 const productEditRender = async (req, res) => {
 
     // Validaciones
-    // await check('productName').notEmpty().withMessage('El nombre del producto no puede estar vacio').run(req)
-    // await check('manufacturer').notEmpty().withMessage('Debes seleccionar el nombre de un fabricante').run(req)
-    // await check('model').notEmpty().withMessage('El producto debe tener un modelo').run(req)
-    // await check('features').notEmpty().withMessage('Debes seleccionar las caracteristicas').run(req)
-    // await check('category').notEmpty().withMessage('Debes seleccionar la categoria').run(req)
-    // await check('description').notEmpty().withMessage('La descripcion es necesaria').run(req)
-    // await check('price').notEmpty().withMessage('Debes indicar el precio del producto').run(req)
-    // await check('discount').notEmpty().withMessage('Debes indicar el descuento del producto').run(req)
-    // await check('stock').notEmpty().withMessage('Debes indicar el stock del producto').run(req)
+    await check('productName').notEmpty().withMessage('El nombre del producto no puede estar vacio').run(req)
+    await check('manufacturer').notEmpty().withMessage('Debes seleccionar el nombre de un fabricante').run(req)
+    await check('model').notEmpty().withMessage('El producto debe tener un modelo').run(req)
+    await check('features').notEmpty().withMessage('Debes seleccionar las caracteristicas').run(req)
+    await check('category').notEmpty().withMessage('Debes seleccionar la categoria').run(req)
+    await check('description').notEmpty().withMessage('La descripcion es necesaria').run(req)
+    await check('price').notEmpty().withMessage('Debes indicar el precio del producto').run(req)
+    await check('discount').notEmpty().withMessage('Debes indicar el descuento del producto').run(req)
+    await check('stock').notEmpty().withMessage('Debes indicar el stock del producto').run(req)
 
-    // let resultado = validationResult(req);
+    const { id } = req.params;
 
-    // if(!resultado.isEmpty()) {
+    let resultado = validationResult(req);
 
-    //     const [categories, manufacturers, features] = await Promise.all([
-    //         Category.findAll(),
-    //         Manufacturer.findAll(),
-    //         Features.findAll()
-    //     ]);
+    if (!resultado.isEmpty()) {
 
-    //     return res.render('products/productEdit', {
-    //         categories, 
-    //         manufacturers, 
-    //         features,
-    //         errors: resultado.array(),
-    //         product: req.body
-    //     });
-    // };
+        const [categories, manufacturers, features] = await Promise.all([
+            Category.findAll(),
+            Manufacturer.findAll(),
+            Features.findAll()
+        ]);
+
+        return res.render(`products/productEdit/${id}`, {
+            categories,
+            manufacturers,
+            features,
+            errors: resultado.array(),
+            product: req.body
+        });
+    };
 
     const productId = req.params.id;
     const product = await Product.findByPk(productId, {
         include: [
-        { model: Manufacturer, as: 'manufacturer'},
-        { model: Category, as: 'category'},
-        { model: Features, as: 'feature'}
-    ]});
-    
-    if(!product){
+            { model: Manufacturer, as: 'manufacturer' },
+            { model: Category, as: 'category' },
+            { model: Features, as: 'feature' }
+        ]
+    });
+
+    if (!product) {
         return res.redirect('products/');
     }
 
@@ -178,17 +180,19 @@ const productEditRender = async (req, res) => {
         Features.findAll()
     ])
 
-    return res.render('products/productEdit', {
+    return res.render(`products/productEdit/${id}`, {
         product,
-        categories, 
-        manufacturers, 
+        categories,
+        manufacturers,
         features,
-        // errors: resultado.array(),
+        errors: resultado.array(),
         product
     });
 };
 
 const productEdit = async (req, res) => {
+    const { id } = req.params;
+
     let image = []
 
     if (req.files[0] != undefined) {
@@ -200,58 +204,57 @@ const productEdit = async (req, res) => {
     }
     let images = image.toString();
 
-    // if(!resultado.isEmpty()){
+    if (!resultado.isEmpty()) {
 
-    //     const [categorys, manufacturers, features] = await Promise.all([
-    //         Category.findAll(),
-    //         Manufacturer.findAll(),
-    //         Features.findAll()
-    //     ])
+        const [categorys, manufacturers, features] = await Promise.all([
+            Category.findAll(),
+            Manufacturer.findAll(),
+            Features.findAll()
+        ])
 
-    //     return res.render('products/productEdit', {
-    //         categorys, 
-    //         manufacturers, 
-    //         features,
-    //         datos: req.body
-    //     });
-    // };
-
-    const { id } = req.params;
+        return res.render(`products/productEdit/${id}`, {
+            categorys,
+            manufacturers,
+            features,
+            datos: req.body
+        });
+    };
 
     // Validacion de que el producto si existe
     const product = await Product.findByPk(id);
 
-    if(!product){
+    if (!product) {
         return res.redirect('/products');
     }
 
     try {
-            const { 
-            productName, 
-            manufacturer,
-            model, 
-            features, 
-            category, 
-            description, 
-            price, 
-            discount, 
-            stock} = req.body;
+        const {
+            productName: name,
+            manufacturer: manufacturer_id,
+            model,
+            features: features_id,
+            category: category_id,
+            description,
+            price,
+            discount,
+            stock 
+        } = req.body;
 
 
         product.set({
-            name: "name",
-            manufacturer_id: 2,
-            model: "model",
-            // features_id, 
-            // category_id, 
-            description: "", 
-            price: 30000, 
-            discount: 0, 
-            stock: 100
+            name,
+            manufacturer_id,
+            model,
+            features_id, 
+            category_id, 
+            description,
+            price,
+            discount,
+            stock
         })
 
         await product.save();
-        res.redirect('/products/')
+        res.redirect('/')
     } catch (error) {
         console.log(error);
     }
@@ -265,7 +268,7 @@ const productDeleteRender = async (req, res) => {
     // Validacion de que el producto si existe
 
     const product = await Product.findByPk(id);
-    if(!product){
+    if (!product) {
         return res.redirect('products/productShop');
     }
 
@@ -284,7 +287,7 @@ const productDelete = async (req, res) => {
 
     const product = await Product.findByPk(id);
 
-    if(!product){
+    if (!product) {
         return res.redirect('/productShop');
     }
 
