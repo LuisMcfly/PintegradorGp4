2062da90545599459a588b57fcf431975b1711ab
ricardogonzalez0607 <ricardogonzalez0607@gmail.com>
2023-01-28 12:32:12 -0500

    DB_Created

--- DB/pegasustech/pegasustech.sql
+++ DB/pegasustech/pegasustech.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `pegasustech` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `pegasustech`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: pegasustech
@@ -14,6 +16,152 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Portátiles'),(2,'Periféricos'),(3,'Computadores de escritorio'),(4,'Gamer');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `features`
+--
+
+DROP TABLE IF EXISTS `features`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `features` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `features`
+--
+
+LOCK TABLES `features` WRITE;
+/*!40000 ALTER TABLE `features` DISABLE KEYS */;
+INSERT INTO `features` VALUES (1,'RAM 8GB | SSD 128GB'),(2,'RAM 16GB | SSD 256GB'),(3,'RAM 16GB | SSD 512GB'),(4,'RAM 32GB | SSD 1TB'),(5,'RAM 32GB | SSD 2TB'),(6,'Suiches RED'),(7,'Suiches BLUE'),(8,'Suiches BROWN'),(9,'24\"'),(10,'27\"'),(11,'32\"');
+/*!40000 ALTER TABLE `features` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `invoiceNumber` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `totalPrice` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `invoices_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturers`
+--
+
+DROP TABLE IF EXISTS `manufacturers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manufacturers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturers`
+--
+
+LOCK TABLES `manufacturers` WRITE;
+/*!40000 ALTER TABLE `manufacturers` DISABLE KEYS */;
+INSERT INTO `manufacturers` VALUES (1,'Dell'),(2,'Samsung'),(3,'HP'),(4,'Lenovo'),(5,'Alienware'),(6,'Asus');
+/*!40000 ALTER TABLE `manufacturers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `discount` int(11) NOT NULL DEFAULT 0,
+  `stock` int(11) NOT NULL DEFAULT 0,
+  `images` varchar(255) NOT NULL DEFAULT 'defaultProductImage.png',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `features_id` int(11) DEFAULT NULL,
+  `manufacturer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  KEY `features_id` (`features_id`),
+  KEY `manufacturer_id` (`manufacturer_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`features_id`) REFERENCES `features` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Portátil Asus Vivobook Pro OLED 15.6 Pulgadas Intel Core i7 16GB 512GB','Vivobook Pro','Portátil Asus Vivobook Pro OLED 15.6 Pulgadas Intel Core i7 16GB 512GB',6500000,5,10,'Asus.jpg','2023-01-28 16:06:02','2023-01-28 16:26:34',1,3,1),(2,'Portátil Lenovo 14 Pulgadas Intel Core i7 16GB 512GB','Quarzs','Portátil Lenovo 14 Pulgadas Intel Core i7 16GB 512GB',3400000,10,5,'Lenovo.png','2023-01-28 16:29:21','2023-01-28 16:46:06',1,3,4),(3,'Portatil Dell Inspiron15 5510 15,6 Ci7 8Gb 128Gb','Inspiron15 5510','Portatil Dell Inspiron15 5510 15,6 Ci7 8Gb 128Gb',2000000,7,8,'Dell.png','2023-01-28 16:35:49','2023-01-28 16:49:07',1,1,1),(4,'Alienware M15 R7','M15 R7','La laptop basada en Intel de 15 pulgadas más potente de Alienware hasta el momento.',10500000,3,10,'AlienWare.png','2023-01-28 16:37:08','2023-01-28 16:51:34',1,5,5),(5,'Redragon S101','S101','Combo de teclado y mouse para juegos con cable Teclado para juegos retroiluminado RGB',500000,2,5,'Combo1.jpg','2023-01-28 16:40:47','2023-01-28 16:54:38',4,6,2),(6,'Monitor para juegos UltraGear™ Full HD IPS 1 ms (GtG) de 24\"','UltraGear™','Monitor para juegos UltraGear™ Full HD IPS 1 ms (GtG) de 24\"',1200000,5,8,'PantallaGamer.png','2023-01-28 16:57:38','2023-01-28 17:01:00',4,9,1),(7,'Pc Torre Gamer Power X08','X08 AMD Atholn','Pc Torre Gamer Power X08 Amd Athlon 3000G Ssd 240Gb Ram 16Gb Led 22 Pulgadas Full Hd',5000000,5,30,'PC_escritorio.png','2023-01-28 17:04:32','2023-01-28 17:09:07',1,2,1),(8,'Set Gamer Teclado Led + Mouse Inalambrico Usb Recargable','TR5800','Set Gamer Teclado Led + Mouse Inalambrico Usb Recargable',150000,8,30,'combo2.png','2023-01-28 17:07:19','2023-01-28 17:09:49',4,6,3);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -59,4 +207,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-28 12:10:45
+-- Dump completed on 2023-01-28 12:29:06
